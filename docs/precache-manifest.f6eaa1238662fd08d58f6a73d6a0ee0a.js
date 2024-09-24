5569841f6dfdceda75a1ee1f031dd35745970544
nisshimura <t-nishimura@keio.jp>
2024-09-25 01:22:16 +0900

    automated commit

---
+++ docs/precache-manifest.f6eaa1238662fd08d58f6a73d6a0ee0a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a9e02929ad4f7b189b58ff1a9b277d",
+    "url": "/kotomi-tanaka/index.html"
+  },
+  {
+    "revision": "3beb3fe5546aceb6f699",
+    "url": "/kotomi-tanaka/static/css/main.b370388c.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/kotomi-tanaka/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/kotomi-tanaka/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "3beb3fe5546aceb6f699",
+    "url": "/kotomi-tanaka/static/js/main.06b2abab.chunk.js"
+  },
+  {
+    "revision": "b3cae338463806268941",
+    "url": "/kotomi-tanaka/static/js/runtime~main.4915f028.js"
+  },
+  {
+    "revision": "d486d6d465fddfe5dce845589f0de970",
+    "url": "/kotomi-tanaka/static/media/S__191832126_0.d486d6d4.jpg"
+  },
+  {
+    "revision": "66db3b859382e95ce525e32ca83db8e4",
+    "url": "/kotomi-tanaka/static/media/S__191832128_0.66db3b85.jpg"
+  },
+  {
+    "revision": "3c2f0fbaf6969edec3bcd117311cd865",
+    "url": "/kotomi-tanaka/static/media/S__191832129_0.3c2f0fba.jpg"
+  },
+  {
+    "revision": "b7749d7403d2b9865bb6edb11c436419",
+    "url": "/kotomi-tanaka/static/media/S__191832130_0.b7749d74.jpg"
+  },
+  {
+    "revision": "279b19fd929fdf35309845305d12461d",
+    "url": "/kotomi-tanaka/static/media/S__191832131_0.279b19fd.jpg"
+  },
+  {
+    "revision": "3a5535403a718868325f95ed6cac8d90",
+    "url": "/kotomi-tanaka/static/media/S__191832132_0.3a553540.jpg"
+  },
+  {
+    "revision": "56fb1b51afbc118a40a0bfc7910d47fc",
+    "url": "/kotomi-tanaka/static/media/S__191832133_0.56fb1b51.jpg"
+  },
+  {
+    "revision": "61316fe49cdd763defda81f1c63c196a",
+    "url": "/kotomi-tanaka/static/media/S__191832134_0.61316fe4.jpg"
+  },
+  {
+    "revision": "a5c2a5f49d45325b202be8d51c5a05f5",
+    "url": "/kotomi-tanaka/static/media/S__191832135_0.a5c2a5f4.jpg"
+  },
+  {
+    "revision": "d3b1c535235d000d18c9f38ee26b20b5",
+    "url": "/kotomi-tanaka/static/media/S__191832136_0.d3b1c535.jpg"
+  },
+  {
+    "revision": "8448155fd47e0eaaccf9c44422916be5",
+    "url": "/kotomi-tanaka/static/media/S__191832147_0.8448155f.jpg"
+  },
+  {
+    "revision": "7990fe1ffbf43e3421e3e0256f85d832",
+    "url": "/kotomi-tanaka/static/media/S__191832149_0.7990fe1f.jpg"
+  },
+  {
+    "revision": "621e75b010451391532dc587e8d6e7c9",
+    "url": "/kotomi-tanaka/static/media/S__191840259_0.621e75b0.jpg"
+  },
+  {
+    "revision": "45e0653b924a77f1f28c021a09ac68ae",
+    "url": "/kotomi-tanaka/static/media/S__191840261_0.45e0653b.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react'
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [portfolioText, setPortfolioText] = useState('');
+  const [demoText, setDemoText] = useState('');
+  const [memosText, setMemosText] = useState('');
+  const [clothingText, setClothingText] = useState('');
+  const [demoImages, setDemoImages] = useState([]);
+  const [memosImages, setMemosImages] = useState([]);
+  const [clothingImages, setClothingImages] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/text/portfolio.txt')
+      .then(response => response.text())
+      .then(data => setPortfolioText(data));
+
+    fetch('/data/text/demo.txt')
+      .then(response => response.text())
+      .then(data => setDemoText(data));
+
+    fetch('/data/text/memos.txt')
+      .then(response => response.text())
+      .then(data => setMemosText(data));
+
+    fetch('/data/text/clothing.txt')
+      .then(response => response.text())
+      .then(data => setClothingText(data));
+
+    // 画像の読み込み
+    const importAll = (r) => r.keys().map(r);
+    setDemoImages(importAll(require.context('../public/data/images/demo', false, /\.(png|jpe?g|svg)$/)));
+    setMemosImages(importAll(require.context('../public/data/images/memos', false, /\.(png|jpe?g|svg)$/)));
+    setClothingImages(importAll(require.context('../public/data/images/clothing', false, /\.(png|jpe?g|svg)$/)));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>服飾学生のポートフォリオ</h1>
+        <p>{portfolioText}</p>
       </header>
+      <section className="section">
+        <h2>学校での作業風景</h2>
+        <p>{demoText}</p>
+        <div className="image-gallery">
+          {demoImages.map((image, index) => (
+            <img key={index} src={image} alt={`Demo ${index + 1}`} />
+          ))}
+        </div>
+      </section>
+      <section className="section">
+        <h2>作りたい服の絵</h2>
+        <p>{memosText}</p>
+        <div className="image-gallery">
+          {memosImages.map((image, index) => (
+            <img key={index} src={image} alt={`Memo ${index + 1}`} />
+          ))}
+        </div>
+      </section>
+      <section className="section">
+        <h2>実際に作った服</h2>
+        <p>{clothingText}</p>
+        <div className="image-gallery">
+          {clothingImages.map((image, index) => (
+            <img key={index} src={image} alt={`Clothing ${index + 1}`} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
